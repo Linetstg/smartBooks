496310e7e8c8b157d834552f915138c8a6beaec6
Taras Lynets <linetstg@gmail.com>
2022-01-26 10:30:20 +0200

    add throw error for fetch

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,7 +7,13 @@ export async function getBooks(
   ) {
 
   const books = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${inputBook}+subject:${filter}&orderBy=${sorting}&startIndex=${startCount}&maxResults=${maxCount}&key=AIzaSyBBA37PHb5UjrZfGBvNsHUF8xct7TRybJ8`)
-    .then(result => result.json())
+    .then(response => {
+      if(!response.ok) {
+        throw new Error(`Server is not response ${response.status}`);
+      }
+
+      return response.json();
+    });
 
     return books;
 }
